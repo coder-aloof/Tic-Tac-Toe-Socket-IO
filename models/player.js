ab6a62486cb858f41cc6dda8f57d763a13503c49
Nirmal Kumar <codingboredom@gmail.com>
2020-02-27 23:03:49 +0530

    added new variable to hold 1 or -1 for every player

--- models/player.js
+++ models/player.js
@@ -1,8 +1,9 @@
-function Player(id, count, socket, symbol) {
+function Player(id, count, socket, symbol, number) {
     this.id = id;
     this.count = count;
     this.socket = socket;
     this.symbol = symbol;
-}
+    this.number = number;
+};
 
 module.exports = Player;
