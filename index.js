fa13cfd32288ff3e0598985c646515a0967a0a93
Nirmal Kumar <codingboredom@gmail.com>
2020-03-02 00:04:26 +0530

    Leaving rooms and archiving games complete

--- index.js
+++ index.js
@@ -20,26 +20,28 @@ server.listen(port, () => {
     console.log('listening on Port : ', port);
 });
 
-let games = [];
+let rooms = []; // to hold games
+let archivedRooms = []; // to hold games that were ended
 
 io.on('connection', (socket) => {
     let game;
     socket.on('join', () => {
-        if (games.length === 0 || games[games.length-1].getPlayersCount() === 2) {
+        if (rooms.length === 0 || rooms[rooms.length-1].getPlayersCount() === 2) {
             game = new Game(uuid());
-            games.push(game);
+            rooms.push(game);
+            console.log(`Total Rooms ${rooms.length}`);
         } else {
-            game = games[games.length - 1];
+            game = rooms[rooms.length - 1];
         }
         socket.roomId = game.getRoomId();
         socket.join(socket.roomId);
         game.addPlayer(socket.id, socket, symbol[game.getPlayersCount()], numbers[game.getPlayersCount()]);
+        if (game.getPlayersCount() === 1) {
+            socket.emit('message', 'Waiting for Player');
+        }
         if (game.getPlayersCount() === 2) {
             io.to(socket.roomId).emit('empty-message', 'Game Starts');
             game.setStatus(1); // In progress
-        }
-        if (game.getPlayersCount() === 1) {
-            socket.emit('message', 'Waiting for Player');
         }
     });
     socket.on('make-move', (data) => {
@@ -54,17 +56,17 @@ io.on('connection', (socket) => {
             if (game.getPositions().indexOf(data.position) === -1) {
                 game.addPosition(data.position);
                 const symbol = game.getCurrentPlayer().symbol;
-                game.getCurrentPlayerSocket().emit('move-made', {
+                socket.emit('move-made', {
                     position: data.position,
                     symbol: symbol
                 });
-                game.getOppositionSocket().emit('move-made', {
+                socket.to(socket.roomId).emit('move-made', {
                     position: data.position,
                     symbol: symbol
                 });
                 if (game.updateBoard(data.position, game.getCurrentPlayer().number)) {
-                    game.getCurrentPlayerSocket().emit('result', 'You Win');
-                    game.getOppositionSocket().emit('result', 'You Lost');
+                    socket.emit('result', 'You Win');
+                    socket.to(socket.roomId).emit('result', 'You Lost');
                 }
                 if (game.checkDraw()) {
                     io.to(socket.roomId).emit('result', 'Draw');
@@ -80,11 +82,22 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log(socket.id, 'disconnected');
         socket.leave(socket.roomId);
-        if (game.getStatus() !== 2) {
+        if (game.getStatus() === 0 && game.getCurrentPlayersCount() === 1) {
+            // when the game has not started yet but the only user in the room disconnects
+            game.playerLeft(socket, false);
+            rooms.splice(game, 1);
+        } else if (game.getStatus() === 1) {
+            // when the game is in progress but one user leaves the room
             game.playerLeft(socket, true);
             io.to(socket.roomId).emit('result', 'Opponent Left! You Win');
-        } else {
+        } else if (game.getStatus() === 2 || game.getCurrentPlayersCount() === 2) {
+            // when the game is over and one user leaves the room
             game.playerLeft(socket, false);
+        } else if (game.getStatus() === 2 || game.getCurrentPlayersCount() === 1) {
+            // when the game is over and the other user also leaves the room
+            game.playerLeft(socket, false);
+            rooms.splice(game,1);
+            archivedRooms.push(game);
         }
     });
 });
